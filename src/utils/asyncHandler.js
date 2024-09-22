45da08340411c8f405f817cd31774d5ccad3ed04
Deepak-Saini <1999onlinejob@gmail.com>
2024-09-22 18:21:28 +0530

    utils code dicusion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,35 @@
+  
+  const asyncHandler = (requestHandler)=>{
+
+    (req, res, next)=>{
+        process.resolve((requestHandler(req, res, next))).catch((error)=>next(error))
+    }
+
+ 
+
+  }
+
+
+
+
+export{ asyncHandler }
+
+
+
+
+
+
+//Second way 
+
+// const asyncHandler = (fn)=> async (req, res, next)=>{
+//     try {
+//         await fn(req, res, next)
+        
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+
+// }
